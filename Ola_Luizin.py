377a4bfd47183afdf6d1ab1adaa64dd1a47657f9
Luiz Gomez <luizinchavao@gmail.com>
2024-07-04 01:45:13 -0300

    Modulo 1 - Aula 6 - Booleanos e Operadores de Comparaçao

--- Ola_Luizin.py
+++ Ola_Luizin.py
@@ -1,17 +1,19 @@
-# AULA 5 - Formataçao em String, listas e Slicing
-a = ["Olá", "Luizin"] # Variavel unica que contém 2 valores
-# len = Vai dizer qual o tamanho de algum elemento
-# Seleçao de lista, quando for selecionar algo no fim da lista: a = [-1]-> vai ler o último elemento
-# Seleçao de intervalo na lista com dois pontos :
-# Ele vai permitir que faça cortes na lista.
-# Ex: a[1:4]-> vai ler o segundo elemento, o terceiro e nao vai ler o quarto elemento da lista.
-# Uma string também é iteiravel de caracteres, é a soma de todas as letras e dos espaços que existirem
-# Tudo que fiz com listas, posso fazer com strings, posso perguntar o tamanho com (len)
-# Ou posso selecionar caractere de maneira individual com [] ex: b[2] ou b[0:4]
-# O igual = é para definir a variavel
-# Uma pincelada no conceito de Slicing é importante para que consiga trabalhar com Dashboard depois
-# Slicing para podermos fazer manipulaçao de dados.
-
-
+# AULA 6 - Booleanos e Operadores de Comparaçao
+# O "sim e o nao" de uma linguagem de programaçao. O Verdadeiro ou Falso.
+# True e False = Entender operar True e False é muito importante. Type(Ele lê o tipo)
+# Ferramentas: Operadores de Comparaçao Símbolo de maior > e menor <
+# Faço uma pergunta para o computador a resposta dele é: Verdadeiro ou Falso
+a = 3 > 2   # É maior do que 2 ?
+b = 3 < 2   # É menor do que 2 ?
+c = 2 >= 2  # É maior ou igual do que 2 ?
+d = 2 <= 2  # É menor ou igual do que 2 ?
+e = 2 != 2
+f = 4 > 20# É diferente do que 2 ? #Ver mais detalhes sobre lista, strings.
+False       #Sao Operadores de comparaçao em cadeia que vamos combinar apenas com Boolianos
+True        #Principais tipos de Comparações. Misturar essas comparações,
+#Com outros elementos como AND e OR Ex: True and True -> Vai retornar verdadeiro
+#Vamos utilizar parametros para agrupar essas operações (3>2) and (2>1) -> É verdadeiro
+# O AND -> Ele retorna apenas se todas expressões estiverem falsas
+#No caso do OR -> Ele é mais Permissivo, ele permite, derrepente que uma das expressões sejam falsas.
 
 
