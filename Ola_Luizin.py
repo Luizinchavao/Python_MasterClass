6361d711e436f3d9da9b056b3091a918315921ae
Luiz Gomez <luizinchavao@gmail.com>
2024-07-03 21:02:15 -0300

    Módulo 1 - Aula 3 - Números e REPL - Aula 4 - Strings e Variáveis

---
+++ Ola_Luizin.py
@@ -0,0 +1,20 @@
+# AULA 4  Strings e Variáveis ok
+
+b = "Ola "
+print(b + "Luizin")
+
+# Sem Variáveis
+print(3,14 * 5**2)
+
+# Com Variáveis
+pi = 3.14
+raio = 5
+raio_ao_quadrado = raio ** 2
+
+print(pi*raio_ao_quadrado)
+
+
+
+
+
+
