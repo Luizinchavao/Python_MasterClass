c4aa3302dc2e0afc7fce179aaed4dadd903c204e
Luiz Gomez <luizinchavao@gmail.com>
2024-07-04 03:23:12 -0300

    Modulo 1 - Aula 7 - Estrutura de Controle de Fluxo - If, Elif e Else

--- Ola_Luizin.py
+++ Ola_Luizin.py
@@ -1,19 +1,12 @@
-# AULA 6 - Booleanos e Operadores de Comparaçao
-# O "sim e o nao" de uma linguagem de programaçao. O Verdadeiro ou Falso.
-# True e False = Entender operar True e False é muito importante. Type(Ele lê o tipo)
-# Ferramentas: Operadores de Comparaçao Símbolo de maior > e menor <
-# Faço uma pergunta para o computador a resposta dele é: Verdadeiro ou Falso
-a = 3 > 2   # É maior do que 2 ?
-b = 3 < 2   # É menor do que 2 ?
-c = 2 >= 2  # É maior ou igual do que 2 ?
-d = 2 <= 2  # É menor ou igual do que 2 ?
-e = 2 != 2
-f = 4 > 20# É diferente do que 2 ? #Ver mais detalhes sobre lista, strings.
-False       #Sao Operadores de comparaçao em cadeia que vamos combinar apenas com Boolianos
-True        #Principais tipos de Comparações. Misturar essas comparações,
-#Com outros elementos como AND e OR Ex: True and True -> Vai retornar verdadeiro
-#Vamos utilizar parametros para agrupar essas operações (3>2) and (2>1) -> É verdadeiro
-# O AND -> Ele retorna apenas se todas expressões estiverem falsas
-#No caso do OR -> Ele é mais Permissivo, ele permite, derrepente que uma das expressões sejam falsas.
+idade = 18
+possui_carteira = False
+
+if(idade >= 18) and possui_carteira:
+    print("Você pode dirigir")
+elif possui_carteira:
+    print("Você apenas possui carteira. Espere a idade")
+else:
+    print("Você nao pode dirigir")
+
 
 
