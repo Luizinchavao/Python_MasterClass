6fc5a642e5bdef96072fc7300f5c84c0fb0c7eb0
Luiz Gomez <luizinchavao@gmail.com>
2024-07-03 23:41:38 -0300

    Modulo 1 - Aula 5 - Formataçao em String, listas e Slicing

--- Ola_Luizin.py
+++ Ola_Luizin.py
@@ -1,19 +1,16 @@
-# AULA 4  Strings e Variáveis ok
-
-b = "Ola "
-print(b + "Luizin")
-
-# Sem Variáveis
-print(3,14 * 5**2)
-
-# Com Variáveis
-pi = 3.14
-raio = 5
-raio_ao_quadrado = raio ** 2
-
-print(pi*raio_ao_quadrado)
-
-
+# AULA 5 - Formataçao em String, listas e Slicing
+a = ["Olá", "Luizin"] # Variavel unica que contém 2 valores
+# len = Vai dizer qual o tamanho de algum elemento
+# Seleçao de lista, quando for selecionar algo no fim da lista: a = [-1]-> vai ler o último elemento
+# Seleçao de intervalo na lista com dois pontos :
+# Ele vai permitir que faça cortes na lista.
+# Ex: a[1:4]-> vai ler o segundo elemento, o terceiro e nao vai ler o quarto elemento da lista.
+# Uma string também é iteiravel de caracteres, é a soma de todas as letras e dos espaços que existirem
+# Tudo que fiz com listas, posso fazer com strings, posso perguntar o tamanho com (len)
+# Ou posso selecionar caractere de maneira individual com [] ex: b[2] ou b[0:4]
+# O igual = é para definir a variavel
+# Uma pincelada no conceito de Slicing é importante para que consiga trabalhar com Dashboard depois
+# Slicing para podermos fazer manipulaçao de dados.
 
 
 
