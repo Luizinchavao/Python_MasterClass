df74b42cbce586359016beb37993fe70d6512210
Luiz Gomez <luizinchavao@gmail.com>
2024-07-05 23:42:34 -0300

    Modulo 1 - Aula 8 - For e Range

--- Ola_Luizin.py
+++ Ola_Luizin.py
@@ -1,12 +1,12 @@
-idade = 18
-possui_carteira = False
-
-if(idade >= 18) and possui_carteira:
-    print("Você pode dirigir")
-elif possui_carteira:
-    print("Você apenas possui carteira. Espere a idade")
-else:
-    print("Você nao pode dirigir")
-
+# Isso aqui o for vai trocando o valor dessa variavel x, vai ser descartada depois
+# Combinar o iteravel, que é uma lista por exemplo e vamos usar o for para decompor ele.
+# E ao mesmo tempo que ele vai decompondo esse nosso iteravel, ele vai executando o trecho de código embaixo.
+# Range, vai gerar, criar uma sequência de números de acordo com o parametro que vamos colocar aqui.
+# Primeiro Parametro - Valor inicial, vou começar do 0
+# Segundo  Parametro - Vamos dizer onde ele vai parar ex: no 10
+# Terceiro Parametro - É o Step - que é de quanto em quanto ele vai andando para chegar, para ir do ínicio até o fim ex: 1
+# Ele vai iniciar no 0, terminar no 9, porque ele para um antes do 10, e andando de um e um.
+for x in range(10): # Troquie o step por 2 # Se nao informar nada no start, ele sabe que é 0.Se nao informar nada no step, ele entende 1
+    print(x)
 
 
